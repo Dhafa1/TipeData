6c3d1d5c1c94d0400807015350acdbac3a675997
Lenovo <Lenovo@LAPTOP-DF5HQVVQ>
2024-07-02 11:44:51 +0700

    Add project files.

---
+++ TipeData/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TipeData
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Numerik
+            int umur = 16;
+            float tinggi = 170.5f;
+            Console.WriteLine("Umur: " + umur);
+            Console.WriteLine("Tinggi: " + tinggi);
+
+            // String
+            string nama = "Budi";
+            Console.WriteLine("Nama: " + nama);
+
+            // Tanggal
+            DateTime hariIni = DateTime.Now;
+            Console.WriteLine("Hari ini: " + hariIni.ToString("dd/MM/yyyy"));
+
+            // Boolean
+            bool isLulus = true;
+            Console.WriteLine("Apakah lulus? " + isLulus);
+        }
+    }
+}
